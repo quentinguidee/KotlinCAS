183dd471f3fdc605c016caa5d3afeeb93b4cf2a4
Quentin Guidée <quentin.guidee@gmail.com>
2020-09-10 22:38:25 +0000

    [algebra] Specials InfiniteSet

--- src/commonMain/kotlin/algebra/sets/InfiniteSet.kt
+++ src/commonMain/kotlin/algebra/sets/InfiniteSet.kt
@@ -1,12 +1,9 @@
 package algebra.sets
 
 import core.expressions.Expression
+import core.expressions.values.*
 
-class InfiniteSet(var symbol: String) : Set() {
-    override fun contains(expression: Expression): Boolean {
-        TODO("Not yet implemented")
-    }
-
+abstract class InfiniteSet(var symbol: String) : Set() {
     override fun toString(): String {
         return symbol
     }
@@ -21,5 +18,41 @@ class InfiniteSet(var symbol: String) : Set() {
             is InfiniteSet -> other.symbol == symbol
             else -> false
         }
+    }
+}
+
+class ComplexSet : InfiniteSet("ℂ") {
+    override fun contains(expression: Expression): Boolean {
+        return expression is Complex || expression is Real || expression is Imaginary
+    }
+}
+
+class ImaginarySet : InfiniteSet("\uD835\uDD40") {
+    override fun contains(expression: Expression): Boolean {
+        return expression is Imaginary
+    }
+}
+
+class RationalSet : InfiniteSet("ℚ") {
+    override fun contains(expression: Expression): Boolean {
+        return expression is Rational
+    }
+}
+
+class RealSet : InfiniteSet("ℝ") {
+    override fun contains(expression: Expression): Boolean {
+        return expression is Real
+    }
+}
+
+class IntegerSet : InfiniteSet("ℤ") {
+    override fun contains(expression: Expression): Boolean {
+        return expression is Integer
+    }
+}
+
+class NaturalSet : InfiniteSet("ℕ") {
+    override fun contains(expression: Expression): Boolean {
+        return expression is Natural
     }
 }
--- src/commonTest/kotlin/algebra/SetsTests.kt
+++ src/commonTest/kotlin/algebra/SetsTests.kt
@@ -1,11 +1,11 @@
 package algebra
 
-import algebra.sets.EmptySet
-import algebra.sets.FiniteSet
-import core.expressions.values.Integer
+import algebra.sets.*
+import core.expressions.values.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFails
+import kotlin.test.assertTrue
 
 class SetsTests {
     @Test
@@ -15,5 +15,12 @@ class SetsTests {
         finiteSet.add(Integer(5))
         finiteSet.addAll(Integer(2), Integer(3), Integer(4))
         assertEquals("{2,5,3,4}", finiteSet.toString())
+    }
+
+    internal fun testInfiniteSet() {
+        assertTrue(RealSet().contains(Real(1)))
+        assertTrue(RealSet().contains(Natural(1)))
+        assertTrue(ComplexSet().contains(Natural(1)))
+        assertTrue(ComplexSet().contains(Complex(Real(1), Imaginary(Real(2)))))
     }
 }
